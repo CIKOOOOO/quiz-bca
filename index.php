186a554af61bcd5cf01a51b46701943e06fedff5
Andrew <abedcreen@gmail.com>
2019-03-29 15:02:58 +0700

    Input Show Total Score, Fixing Progress Bar Bug

--- index.php
+++ index.php
@@ -42,7 +42,8 @@
 					<div class="nickbox">
 						<input type="text" id="inputNickname" placeholder="masukan nickname" name="nickname" required> <br>
 					</div>
-					<button id="btn" onclick="submitClick()">Submit</button>
+					<button id="btn" onclick="submitClick()">submit</button>
+					<button id="btn_total" onclick="showTotalScore()">show total score</button>
 					<!-- <input id="btn" type="submit" value="submit"> -->
 				<!-- </form> -->
 				
